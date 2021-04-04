b39aa3b4a8fc6033ec3224f30c06d0da03c19fa2
Kalon Hinds <kjhinds@gmail.com>
2021-04-03 20:07:33 -0700

    Refactor legislators method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -15,12 +15,10 @@ def legislators_by_zipcode(zip)
     legislators = civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
-  rescue
+    legislators.officials.map(&:name).join(', ')
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
